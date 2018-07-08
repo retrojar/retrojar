012a0cf1ffac779b8565cf0867ce42673149910b
gazsi.bence <gazsi.bence@virgo.hu>
2018-07-08 20:42:51 +0200

    sw fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
-    name: 'RetroMemo'
+    name: 'RetroMemo',
+    workboxOptions: {
+      "globDirectory": "public/",
+      "globPatterns": [
+          "**/*.{html,ttf,woff,woff2,ico,js,css}"
+      ],
+      "swSrc": "./src/sw.js",
+      "swDest": "./public/sw.js",
+    }
   },
 
-  lintOnSave: undefined
+  lintOnSave: true
 }

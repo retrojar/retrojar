d75e5129de803315df81f41f9904c5721c10f99d
bondor.janos <bondor.janos@virgo.hu>
2018-08-02 10:36:03 +0200

    apihost

--- .circleci/aws_deploy.sh
+++ .circleci/aws_deploy.sh
@@ -6,19 +6,22 @@ $(aws ecr get-login)
 docker tag $DOCKER_IMAGE $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE:$CIRCLE_SHA1
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE:$CIRCLE_SHA1
 
-task_template='[
-{
-  "name": "%s",
-  "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
-  "essential": true,
-  "memoryReservation": 64,
-  "portMappings": [
-    {
-      "containerPort": %s
-    }
-  ]
-}
-]'
+task_template='[{
+    "name": "%s",
+    "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
+    "essential": true,
+    "memoryReservation": 64,
+    "portMappings": [
+        {
+            "containerPort": %s
+        }
+    ],
+    "environment": [
+        {
+            "API_HOST": "https://api.retrojar.top",
+        }
+    ],
+}]'
 task_def=$(printf "$task_template" $TASK $AWS_ACCOUNT_ID $AWS_REGION $DOCKER_IMAGE $CIRCLE_SHA1 $PORT)
 echo "$task_def"
 

fbe25137c74b4c7405038d8289932c2aba08f617
bondor.janos <bondor.janos@virgo.hu>
2018-09-09 20:12:02 +0200

    aws refact

--- .circleci/aws_deploy.sh
+++ .circleci/aws_deploy.sh
@@ -3,17 +3,29 @@
 aws configure set region $AWS_REGION
 $(aws ecr get-login)
 
-docker tag $DOCKER_IMAGE $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE:$CIRCLE_SHA1
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE:$CIRCLE_SHA1
+server_img=$(printf "%s.dkr.ecr.%s.amazonaws.com/server:%s" $AWS_ACCOUNT_ID $AWS_REGION $CIRCLE_SHA1)
+client_img=$(printf "%s.dkr.ecr.%s.amazonaws.com/client:%s" $AWS_ACCOUNT_ID $AWS_REGION $CIRCLE_SHA1)
 
-task_template='[{
-    "name": "%s",
-    "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
+docker tag server $server_img
+docker tag client $client_img
+docker push $server_img
+docker push $client_img
+
+task_template='
+[{
+    "name": "server",
+    "image": "%s",
+    "essential": true,
+    "memoryReservation": 64
+},
+{
+    "name": "client",
+    "image": "%s",
     "essential": true,
     "memoryReservation": 64,
     "portMappings": [
         {
-            "containerPort": %s
+            "containerPort": 80
         }
     ],
     "environment": [
@@ -22,14 +34,16 @@ task_template='[{
             "value": "https://api.retrojar.top"
         }
     ]
-}]'
-task_def=$(printf "$task_template" $TASK $AWS_ACCOUNT_ID $AWS_REGION $DOCKER_IMAGE $CIRCLE_SHA1 $PORT)
+}]
+'
+
+task_def=$(printf "$task_template" $server_img $client_img)
 echo "$task_def"
 
-json=$(aws ecs register-task-definition --container-definitions "$task_def" --family "$FAMILY")
+json=$(aws ecs register-task-definition --container-definitions "$task_def" --family "retrojar")
 echo "$json"
 
 revision=$(echo "$json" | grep -o '"revision": [0-9]*' | grep -Eo '[0-9]+')
 echo "$revision"
 
-aws ecs update-service --cluster "$CLUSTER" --service "$SERVICE" --task-definition "$FAMILY":"$revision"
+aws ecs update-service --cluster "default" --service "retrojar" --task-definition "retrojar":"$revision"
